27574a2de04cfc79104240405e35b10d3adf1577
artur <artumingazov@gmail.com>
2025-06-30 01:21:01 +0300

    Add BaseTest.java and remove .DS_Store files from repo

--- src/test/java/tests/Base/BaseTest.java
+++ src/test/java/tests/Base/BaseTest.java
@@ -3,29 +3,44 @@ package tests.Base;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import utils.DriverUtils;
-import pages.CreateWalletPage;
-import pages.EnableNotificationsPage;
-import pages.WalletIsReadyPage;
-import pages.HomePage;
-import pages.ReceivePage;
+import utils.*;
+import pages.*;
 
 public class BaseTest {
 
     protected CreateWalletPage createWalletPage;
     protected EnableNotificationsPage enableNotificationsPage;
-    protected WalletIsReadyPage walletIsReadyPage;
+    protected WalletReadyPage walletReadyPage;
     protected HomePage homePage;
     protected ReceivePage receivePage;
+    protected PinCodePage pinCodePage;
+    protected ImportWalletPage importWalletPage;
+    protected SendPage sendPage;
 
     @BeforeMethod
     public void setUp() {
         DriverUtils.initializeDriver();
+        importWalletPage = new ImportWalletPage();
     }
 
     @AfterMethod
     public void tearDown(ITestResult result) {
         DriverUtils.quitDriver();
+    }
+
+    protected HomePage importWalletFlow() {
+        importWalletPage = createWalletPage.clickImportWalletButton();
+        importWalletPage.waitForImportPageToLoad();
+        
+        importWalletPage.enterSeedPhrase(TestData.TEST_SEED_PHRASE);
+        
+        pinCodePage = importWalletPage.clickImportButton();
+        
+        pinCodePage.enterPinCode(TestData.DEFAULT_PIN_CODE);
+        pinCodePage.enterPinCode(TestData.DEFAULT_PIN_CODE);
+        
+        homePage = walletReadyPage.clickButton();
+        return homePage;
     }
 
 }
